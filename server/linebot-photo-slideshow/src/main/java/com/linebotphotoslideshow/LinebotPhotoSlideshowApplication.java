9792123156c05de8bdc8015ce40b9d4620416434
gucci67 <w.xig.11cj@gmail.com>
2023-04-15 15:41:50 +0900

    springbootのプロジェクト作成

---
+++ server/linebot-photo-slideshow/src/main/java/com/linebotphotoslideshow/LinebotPhotoSlideshowApplication.java
@@ -0,0 +1,13 @@
+package com.linebotphotoslideshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotPhotoSlideshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotPhotoSlideshowApplication.class, args);
+	}
+
+}
